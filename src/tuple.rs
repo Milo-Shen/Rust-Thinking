faf980217cd0f960ea85d31af67a80e9b6073ad0
MiloShen <Milo-Shen@outlook.com>
2022-07-10 16:59:14 +0800

    feat: add example in tuple

--- src/tuple.rs
+++ src/tuple.rs
@@ -11,4 +11,15 @@ pub fn tuple() {
     let six_point_four = x.1;
     let one = x.2;
     println!("{}, {}, {}", five_hundred, six_point_four, one);
+
+    // 元组的使用示例
+    // 元组在函数返回值场景很常用，例如下面的代码，可以使用元组返回多个值
+    let s1 = String::from("hello");
+    let (s2, len) = calculate_length(s1);
+    println!("The length of '{}' is {}.", s2, len);
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); // len() 返回字符串的长度
+    (s, length)
 }
