f92d564fced5bd55396d39d4408c122b022916ec
MiloShen <Milo-Shen@outlook.com>
2022-07-14 21:53:54 +0800

    feat: add Pair example

--- src/learn_trait.rs
+++ src/learn_trait.rs
@@ -113,4 +113,26 @@ pub fn learn_trait() {
         U: Clone + Debug,
     {
     }
+
+    // 使用特征约束有条件地实现方法或特征
+    struct Pair<T> {
+        x: T,
+        y: T,
+    }
+
+    impl<T> Pair<T> {
+        fn new(x: T, y: T) -> Self {
+            Self { x, y }
+        }
+    }
+
+    impl<T: Display + PartialOrd> Pair<T> {
+        fn cmp_display(&self) {
+            if self.x >= self.y {
+                println!("The largest member is x = {}", self.x);
+            } else {
+                println!("The largest member is y = {}", self.y);
+            }
+        }
+    }
 }
