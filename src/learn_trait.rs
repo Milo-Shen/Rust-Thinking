7cabdd5f0e94ee177b8448a414d5e06278174676
MiloShen <Milo-Shen@outlook.com>
2022-07-14 21:03:45 +0800

    feat: add trait example

--- src/learn_trait.rs
+++ src/learn_trait.rs
@@ -8,4 +8,41 @@ pub fn learn_trait() {
     pub trait Summary {
         fn summarize(&self) -> String;
     }
+
+    // 为类型实现特征
+    pub struct Post {
+        pub title: String,   // 标题
+        pub author: String,  // 作者
+        pub content: String, // 内容
+    }
+
+    impl Summary for Post {
+        fn summarize(&self) -> String {
+            format!("文章{}, 作者是{}", self.title, self.author)
+        }
+    }
+
+    pub struct Weibo {
+        pub username: String,
+        pub content: String,
+    }
+
+    impl Summary for Weibo {
+        fn summarize(&self) -> String {
+            format!("{}发表了微博{}", self.username, self.content)
+        }
+    }
+
+    let post = Post {
+        title: "Rust语言简介".to_string(),
+        author: "Sunface".to_string(),
+        content: "Rust棒极了!".to_string(),
+    };
+    let weibo = Weibo {
+        username: "sunface".to_string(),
+        content: "好像微博没Tweet好用".to_string(),
+    };
+
+    println!("{}", post.summarize());
+    println!("{}", weibo.summarize());
 }
