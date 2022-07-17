eef793b46930c7761d7732aefdc373ac8a48f550
MiloShen <Milo-Shen@outlook.com>
2022-07-17 15:24:48 +0800

    feat: add example for hashmap

--- src/hash_map.rs
+++ src/hash_map.rs
@@ -1,1 +1,13 @@
-pub fn hash_map() {}
+use std::collections::HashMap;
+
+pub fn hash_map() {
+    let teams_list = vec![
+        ("中国队".to_string(), 100),
+        ("美国队".to_string(), 10),
+        ("日本队".to_string(), 50),
+    ];
+
+    let teams_map: HashMap<_, _> = teams_list.into_iter().collect();
+
+    println!("{:?}", teams_map)
+}
