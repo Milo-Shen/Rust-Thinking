abb51db8ca33ddfef96b611f23317a7ee9c3f64c
MiloShen <Milo-Shen@outlook.com>
2022-07-17 16:50:57 +0800

    feat: add example for type conversion

--- src/type_conversion.rs
+++ src/type_conversion.rs
@@ -6,5 +6,16 @@ pub fn type_conversion() {
     let a = 3.1 as i8;
     let b = 100_i8 as i32;
     let c = 'a' as u8;
-    println!("{},{},{}", a, b, c)
+    println!("{},{},{}", a, b, c);
+
+    // 内存地址转换为指针
+    let mut values: [i32; 2] = [1, 2];
+    let p1: *mut i32 = values.as_mut_ptr();
+    let first_address = p1 as usize; // 将p1内存地址转换为一个整数
+    let second_address = first_address + 4; // 4 == std::mem::size_of::<i32>()，i32类型占用4个字节，因此将内存地址 + 4
+    let p2 = second_address as *mut i32; // 访问该地址指向的下一个整数p2
+    unsafe {
+        *p2 += 1;
+    }
+    assert_eq!(values[1], 3);
 }
