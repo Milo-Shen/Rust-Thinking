751c1d78d0f270078c68c5a6d803653d2ebd19d3
MiloShen <Milo-Shen@outlook.com>
2022-07-17 15:02:37 +0800

    feat: add vec example

--- src/learn_vec.rs
+++ src/learn_vec.rs
@@ -5,4 +5,25 @@ pub fn learn_vec() {
     // let first = &v[0];
     // v.push(6);
     // println!("The first element is: {}", first);
+
+    // 存储不同类型的元素
+    // 在本节开头，有讲到数组的元素必需类型相同，但是也提到了解决方案：那就是通过使用枚举类型和特征对象来实现不同类型元素的存储。先来看看通过枚举如何实现：
+    #[derive(Debug)]
+    enum IpAddr {
+        V4(String),
+        V6(String),
+    }
+
+    fn show_addr(ip: IpAddr) {
+        println!("{:?}", ip);
+    }
+
+    let v = vec![
+        IpAddr::V4("127.0.0.1".to_string()),
+        IpAddr::V6("::1".to_string()),
+    ];
+
+    for ip in v {
+        show_addr(ip)
+    }
 }
