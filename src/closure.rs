80796a7d6808817a26e028a4ea1dac1bfcc65e89
MiloShen <Milo-Shen@outlook.com>
2022-07-23 16:42:26 +0800

    feat: add example for clousre

--- src/closure.rs
+++ src/closure.rs
@@ -1,1 +1,7 @@
-pub fn closure() {}
+pub fn closure() {
+    // 闭包是一种匿名函数，它可以赋值给变量也可以作为参数传递给其它函数，不同于函数的是，它允许捕获调用者作用域中的值，例如：
+    let x = 1;
+    let sum = |y| x + y;
+    let total = sum(1);
+    println!("total = {total}");
+}
