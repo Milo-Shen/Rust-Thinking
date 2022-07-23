165bfdf8bb13f238ad11d57f0bc2530cb9e31817
MiloShen <Milo-Shen@outlook.com>
2022-07-23 17:01:22 +0800

    feat: add closure example

--- src/closure.rs
+++ src/closure.rs
@@ -71,4 +71,29 @@ pub fn closure() {
 
     // 开始健身
     workout1(intensity, random_number);
+
+    // 闭包实现
+    // 上面提到 intensity 要是变化怎么办，简单，使用闭包来捕获它，这是我们的拿手好戏：
+    fn workout2(intensity: u32, random_number: u32) {
+        let action = || {
+            println!("muuuu.....");
+            thread::sleep(Duration::from_millis(1));
+            intensity
+        };
+
+        if intensity < 25 {
+            println!("今天活力满满，先做 {} 个俯卧撑!", action());
+            println!("旁边有妹子在看，俯卧撑太low，再来 {} 组卧推!", action());
+        } else if random_number == 3 {
+            println!("昨天练过度了，今天还是休息下吧！");
+        } else {
+            println!("昨天练过度了，今天干干有氧，跑步 {} 分钟!", action());
+        }
+    }
+    // 动作次数
+    let intensity = 10;
+    // 随机值用来决定某个选择
+    let random_number = 7;
+    // 开始健身
+    workout2(intensity, random_number);
 }
