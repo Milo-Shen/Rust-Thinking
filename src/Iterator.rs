7bbb1d07a0547ea62c459b3d81cbe150e9490e13
MiloShen <Milo-Shen@outlook.com>
2022-07-24 18:15:15 +0800

    feat: add comments for into_iter, iter, iter_mut

--- src/Iterator.rs
+++ src/Iterator.rs
@@ -67,4 +67,29 @@ pub fn iterator() {
     // 1. into_iter 会夺走所有权
     // 2. iter 是借用
     // 3. iter_mut 是可变借用
+
+    // 其实如果以后见多识广了，你会发现这种问题一眼就能看穿，into_ 之类的，都是拿走所有权，_mut 之类的都是可变借用，剩下的就是不可变借用。
+    let values = vec![1, 2, 3];
+    for v in values.into_iter() {
+        println!("{}", v)
+    }
+    // 下面的代码将报错，因为 values 的所有权在上面 `for` 循环中已经被转移走
+    // println!("{:?}",values);
+    let values = vec![1, 2, 3];
+    let _values_iter = values.iter();
+    // 不会报错，因为 values_iter 只是借用了 values 中的元素
+    println!("{:?}", values);
+    let mut values = vec![1, 2, 3];
+    // 对 values 中的元素进行可变借用
+    let mut values_iter_mut = values.iter_mut();
+    // 取出第一个元素，并修改为0
+    if let Some(v) = values_iter_mut.next() {
+        *v = 0;
+    }
+    // 输出[0, 2, 3]
+    println!("{:?}", values);
+
+    // 具体解释在代码注释中，就不再赘述，不过有两点需要注意的是：
+    // 1 .iter() 方法实现的迭代器，调用 next 方法返回的类型是 Some(&T)
+    // 2 .iter_mut() 方法实现的迭代器，调用 next 方法返回的类型是 Some(&mut T)，因此在 if let Some(v) = values_iter_mut.next() 中，v 的类型是 &mut i32，最终我们可以通过 *v = 0 的方式修改其值
 }
