bc89d217d94cfb6b62e38ca1ed885d112a51f07f
MiloShen <Milo-Shen@outlook.com>
2022-07-24 13:53:42 +0800

    feat: add move example

--- src/closure.rs
+++ src/closure.rs
@@ -218,4 +218,11 @@ pub fn closure() {
         println!("{}", func(3));
         println!("{}", func(4));
     }
+
+    // 如果你想强制闭包取得捕获变量的所有权，可以在参数列表前添加 move 关键字，这种用法通常用于闭包的生命周期大于捕获变量的生命周期时，例如将闭包返回或移入其他线程。
+    let v = vec![1, 2, 3];
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+    handle.join().unwrap();
 }
