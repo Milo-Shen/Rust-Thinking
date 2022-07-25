628cef6c9d8d5317323579085a3bb10d45fca03d
MiloShen <Milo-Shen@outlook.com>
2022-07-25 22:04:25 +0800

    feat: add example for better readability and type alienation

--- src/newtype_and_type_aliases.rs
+++ src/newtype_and_type_aliases.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::ops::Add;
 
 pub fn newtype_and_type_aliases() {
     // newtype
@@ -12,11 +13,39 @@ pub fn newtype_and_type_aliases() {
     // 在之前的章节中，我们有讲过，如果在外部类型上实现外部特征必须使用 newtype 的方式，否则你就得遵循孤儿规则：要为类型 A 实现特征 T，那么 A 或者 T 必须至少有一个在当前的作用范围内。
     // 例如，如果想使用 println!("{}", v) 的方式去格式化输出一个动态数组 Vec，以期给用户提供更加清晰可读的内容，那么就需要为 Vec 实现 Display 特征，但是这里有一个问题： Vec 类型定义在标准库中，Display 亦然，这时就可以祭出大杀器 newtype 来解决：
     struct Wrapper(Vec<String>);
+
     impl fmt::Display for Wrapper {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
             write!(f, "[{}]", self.0.join(", "))
         }
     }
+
     let w = Wrapper(vec![String::from("hello"), String::from("world")]);
     println!("w = {}", w);
+    // 如上所示，使用元组结构体语法 struct Wrapper(Vec<String>) 创建了一个 newtype Wrapper，然后为它实现 Display 特征，最终实现了对 Vec 动态数组的格式化输出。
+
+    // 更好的可读性及类型异化
+    // 首先，更好的可读性不等于更少的代码（如果你学过 Scala，相信会深有体会），其次下面的例子只是一个示例，未必能体现出更好的可读性：
+    struct Meters(u32);
+    impl fmt::Display for Meters {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "目标地点距离你{}米", self.0)
+        }
+    }
+
+    impl Add for Meters {
+        type Output = Self;
+
+        fn add(self, other: Meters) -> Self {
+            Self(self.0 + other.0)
+        }
+    }
+
+    fn calculate_distance(d1: Meters, d2: Meters) -> Meters {
+        d1 + d2
+    }
+
+    let d = calculate_distance(Meters(10), Meters(20));
+    println!("{}", d);
+    // 事实上，除了可读性外，还有一个极大的优点：如果给 calculate_distance 传一个其它的类型，例如 struct MilliMeters(u32);，该代码将无法编译。尽管 Meters 和 MilliMeters 都是对 u32 类型的简单包装，但是它们是不同的类型！
 }
