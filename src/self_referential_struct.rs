29aea4917f665a57c1e04f6cd95fc3f6e1b1a211
Milo-Shen <Milo-Shen@outlook.com>
2022-07-31 21:47:38 +0800

    feat: add WhatAboutThis1 example

--- src/self_referential_struct.rs
+++ src/self_referential_struct.rs
@@ -29,4 +29,38 @@ pub fn self_referential_struct() {
     tricky.nickname = Some(&tricky.name[..4]);
 
     println!("{:?}", tricky);
+
+    // 在某种程度上来说，Option 这个方法可以工作，但是这个方法的限制较多，例如从一个函数创建并返回它是不可能的：
+    // fn creator<'a>() -> WhatAboutThis<'a> {
+    //     let mut tricky = WhatAboutThis {
+    //         name: "Annabelle".to_string(),
+    //         nickname: None,
+    //     };
+    //     tricky.nickname = Some(&tricky.name[..4]);
+
+    //     tricky
+    // }
+
+    // 其实从函数签名就能看出来端倪，'a 生命周期是凭空产生的！
+    // 如果是通过方法使用，你需要一个无用 &'a self 生命周期标识，一旦有了这个标识，代码将变得更加受限，你将很容易就获得借用错误，就连 NLL 规则都没用：
+    #[derive(Debug)]
+    struct WhatAboutThis1<'a> {
+        name: String,
+        nickname: Option<&'a str>,
+    }
+
+    impl<'a> WhatAboutThis1<'a> {
+        fn tie_the_knot(&'a mut self) {
+            self.nickname = Some(&self.name[..4]);
+        }
+    }
+
+    let mut tricky = WhatAboutThis1 {
+        name: "Annabelle".to_string(),
+        nickname: None,
+    };
+    tricky.tie_the_knot();
+
+    // cannot borrow `tricky` as immutable because it is also borrowed as mutable
+    // println!("{:?}", tricky);
 }
