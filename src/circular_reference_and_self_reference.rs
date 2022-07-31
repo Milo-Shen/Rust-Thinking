5b0c210546a6278ea589b7b60b4b11c865097a51
MiloShen <Milo-Shen@outlook.com>
2022-07-31 15:49:25 +0800

    feat: add example for circular_reference_and_self_reference

--- src/circular_reference_and_self_reference.rs
+++ src/circular_reference_and_self_reference.rs
@@ -1,6 +1,52 @@
+use std::{cell::RefCell, rc::Rc};
+
 pub fn circular_reference_and_self_reference() {
     // 循环引用与自引用
     // 实现一个链表是学习各大编程语言的常用技巧，但是在 Rust 中实现链表意味着····Hell，是的，你没看错，Welcome to hell。
     // 链表在 Rust 中之所以这么难，完全是因为循环引用和自引用的问题引起的，这两个问题可以说综合了 Rust 的很多难点，难出了新高度，因此本书专门开辟一章，分为上下两篇，试图彻底解决这两个老大难。
     // 本章难度较高，但是非常值得深入阅读，它会让你对 Rust 的理解上升到一个新的境界。
+
+    // Rust 的安全性是众所周知的，但是不代表它不会内存泄漏。一个典型的例子就是同时使用 Rc<T> 和 RefCell<T> 创建循环引用，最终这些引用的计数都无法被归零，因此 Rc<T> 拥有的值也不会被释放清理。
+    #[derive(Debug)]
+    enum List {
+        Cons(i32, RefCell<Rc<List>>),
+        Nil,
+    }
+
+    use List::{Cons, Nil};
+
+    impl List {
+        fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+            match self {
+                Cons(_, item) => Some(item),
+                Nil => None,
+            }
+        }
+    }
+
+    // 这里我们创建一个有些复杂的枚举类型 List，这个类型很有意思，它的每个值都指向了另一个 List，此外，得益于 Rc 的使用还允许多个值指向一个 List
+    // 如上图所示，每个矩形框节点都是一个 List 类型，它们或者是拥有值且指向另一个 List 的 Cons，或者是一个没有值的终结点 Nil。同时，由于 RefCell 的使用，每个 List 所指向的 List 还能够被修改。
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+    println!("a的初始化rc计数 = {}", Rc::strong_count(&a));
+    println!("a指向的节点 = {:?}", a.tail());
+
+    // 创建`b`到`a`的引用
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+    println!("在b创建后，a的rc计数 = {}", Rc::strong_count(&a));
+    println!("b的初始化rc计数 = {}", Rc::strong_count(&b));
+    println!("b指向的节点 = {:?}", b.tail());
+
+    // 利用RefCell的可变性，创建了`a`到`b`的引用
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("在更改a后，b的rc计数 = {}", Rc::strong_count(&b));
+    println!("在更改a后，a的rc计数 = {}", Rc::strong_count(&a));
+
+    // 下面一行println!将导致循环引用
+    // 我们可怜的8MB大小的main线程栈空间将被它冲垮，最终造成栈溢出
+    // println!("a next item = {:?}", a.tail());
 }
