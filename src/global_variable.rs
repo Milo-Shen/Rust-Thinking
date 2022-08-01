cdf840cfba4555e899ddf203eae57a6b415ffece
Milo-Shen <Milo-Shen@outlook.com>
2022-08-01 22:13:38 +0800

    feat: add box leak example

--- src/global_variable.rs
+++ src/global_variable.rs
@@ -73,4 +73,22 @@ pub fn global_variable() {
             }
         }
     }
+
+    #[derive(Debug)]
+    struct Config {
+        a: String,
+        b: String,
+    }
+    static mut CONFIG: Option<&mut Config> = None;
+
+    let c = Box::new(Config {
+        a: "A".to_string(),
+        b: "B".to_string(),
+    });
+
+    unsafe {
+        // 将`c`从内存中泄漏，变成`'static`生命周期
+        CONFIG = Some(Box::leak(c));
+        println!("{:?}", CONFIG);
+    }
 }
