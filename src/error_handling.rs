f5ea9585b3dbb3554ed2f79b5eb4610b1d5059fd
Milo-Shen <Milo-Shen@outlook.com>
2022-08-05 21:30:30 +0800

    feat: add or/and example

--- src/error_handling.rs
+++ src/error_handling.rs
@@ -5,4 +5,41 @@ pub fn error_handling() {
     // 组合器
     // 在设计模式中，有一个组合器模式，相信有 Java 背景的同学对此并不陌生。
     // 将对象组合成树形结构以表示“部分整体”的层次结构。组合模式使得用户对单个对象和组合对象的使用具有一致性。–GoF <<设计模式>>
+
+    // 与组合起模式有所不同，在 Rust 中，组合器更多的是用于对返回结果的类型进行变换：例如使用 ok_or 将一个 Option 类型转换成 Result 类型。
+
+    // or() 和 and()
+    // 跟布尔关系的与/或很像，这两个方法会对两个表达式做逻辑组合，最终返回 Option / Result。
+
+    // or()，表达式按照顺序求值，若任何一个表达式的结果是 Some 或 Ok，则该值会立刻返回
+    // and()，若两个表达式的结果都是 Some 或 Ok，则第二个表达式中的值被返回。若任何一个的结果是 None 或 Err ，则立刻返回。
+    // 实际上，只要将布尔表达式的 true / false，替换成 Some / None 或 Ok / Err 就很好理解了。
+    let s1 = Some("some1");
+    let s2 = Some("some2");
+    let n: Option<&str> = None;
+
+    let o1: Result<&str, &str> = Ok("ok1");
+    let o2: Result<&str, &str> = Ok("ok2");
+    let e1: Result<&str, &str> = Err("error1");
+    let e2: Result<&str, &str> = Err("error2");
+
+    assert_eq!(s1.or(s2), s1); // Some1 or Some2 = Some1
+    assert_eq!(s1.or(n), s1); // Some or None = Some
+    assert_eq!(n.or(s1), s1); // None or Some = Some
+    assert_eq!(n.or(n), n); // None1 or None2 = None2
+
+    assert_eq!(o1.or(o2), o1); // Ok1 or Ok2 = Ok1
+    assert_eq!(o1.or(e1), o1); // Ok or Err = Ok
+    assert_eq!(e1.or(o1), o1); // Err or Ok = Ok
+    assert_eq!(e1.or(e2), e2); // Err1 or Err2 = Err2
+
+    assert_eq!(s1.and(s2), s2); // Some1 and Some2 = Some2
+    assert_eq!(s1.and(n), n); // Some and None = None
+    assert_eq!(n.and(s1), n); // None and Some = None
+    assert_eq!(n.and(n), n); // None1 and None2 = None1
+
+    assert_eq!(o1.and(o2), o2); // Ok1 and Ok2 = Ok2
+    assert_eq!(o1.and(e1), e1); // Ok and Err = Err
+    assert_eq!(e1.and(o1), e1); // Err and Ok = Err
+    assert_eq!(e1.and(e2), e1); // Err1 and Err2 = Err1
 }
