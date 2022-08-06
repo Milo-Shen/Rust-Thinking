f0718af2c6fda3bad7d7e0e48008dc2cde963927
Milo-Shen <Milo-Shen@outlook.com>
2022-08-06 13:46:02 +0800

    feat: add exmaple for ok_or_else

--- src/error_handling.rs
+++ src/error_handling.rs
@@ -191,4 +191,15 @@ pub fn error_handling() {
 
     assert_eq!(s.ok_or(ERR_DEFAULT), o); // Some(T) -> Ok(T)
     assert_eq!(n.ok_or(ERR_DEFAULT), e); // None -> Err(default)
+
+    // 而 ok_or_else 接收一个闭包作为 Err 参数:
+    let s = Some("abcde");
+    let n: Option<&str> = None;
+    let fn_err_message = || "error message";
+
+    let o: Result<&str, &str> = Ok("abcde");
+    let e: Result<&str, &str> = Err("error message");
+
+    assert_eq!(s.ok_or_else(fn_err_message), o); // Some(T) -> Ok(T)
+    assert_eq!(n.ok_or_else(fn_err_message), e); // None -> Err(default)
 }
