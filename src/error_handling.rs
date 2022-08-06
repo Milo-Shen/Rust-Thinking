a68fcec81f28d3fe9b9233e1ecfccc1d8361acb1
Milo-Shen <Milo-Shen@outlook.com>
2022-08-06 13:24:23 +0800

    feat: add example for map

--- src/error_handling.rs
+++ src/error_handling.rs
@@ -113,4 +113,26 @@ pub fn error_handling() {
     assert_eq!(s1.filter(fn_is_even), n); // Some(3) -> 3 is not even -> None
     assert_eq!(s2.filter(fn_is_even), s2); // Some(6) -> 6 is even -> Some(6)
     assert_eq!(n.filter(fn_is_even), n); // None -> no value -> None
+
+    // map() 和 map_err()
+    // map 可以将 Some 或 Ok 中的值映射为另一个：
+    let s1 = Some("abcde");
+    let s2 = Some(5);
+
+    let n1: Option<&str> = None;
+    let n2: Option<usize> = None;
+
+    let o1: Result<&str, &str> = Ok("abcde");
+    let o2: Result<usize, &str> = Ok(5);
+
+    let e1: Result<&str, &str> = Err("abcde");
+    let e2: Result<usize, &str> = Err("abcde");
+
+    let fn_character_count = |s: &str| s.chars().count();
+
+    assert_eq!(s1.map(fn_character_count), s2); // Some1 map = Some2
+    assert_eq!(n1.map(fn_character_count), n2); // None1 map = None2
+
+    assert_eq!(o1.map(fn_character_count), o2); // Ok1 map = Ok2
+    assert_eq!(e1.map(fn_character_count), e2); // Err1 map = Err2
 }
