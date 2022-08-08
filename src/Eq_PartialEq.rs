30af10be3dfec5155b2e92344b1e50471c641f72
Milo-Shen <Milo-Shen@outlook.com>
2022-08-08 22:21:48 +0800

    feat: add PartialOrd example

--- src/Eq_PartialEq.rs
+++ src/Eq_PartialEq.rs
@@ -1,1 +1,31 @@
-pub fn Eq_PartialEq() {}
+use std::fmt::Display;
+
+pub fn Eq_PartialEq() {
+    // Eq 和 PartialEq
+    // 在 Rust 中，想要重载操作符，你就需要实现对应的特征。
+    // 例如 <、<=、> 和 >= 需要实现 PartialOrd 特征:
+
+    struct Pair<T> {
+        x: T,
+        y: T,
+    }
+
+    impl<T> Pair<T> {
+        fn new(x: T, y: T) -> Self {
+            Self { x, y }
+        }
+    }
+
+    impl<T: Display + PartialOrd> Pair<T> {
+        fn cmp_display(&self) {
+            if self.x >= self.y {
+                println!("The largest member is x = {}", self.x);
+            } else {
+                println!("The largest member is y = {}", self.y);
+            }
+        }
+    }
+
+    let a = Pair::new(1, 1);
+    a.cmp_display();
+}
